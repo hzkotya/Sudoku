2ec9b49da8da8df667a353d8d4f14beb7d0778dd
hzkotya <ladymary712@yandex.com>
2023-04-22 01:24:34 +0300

    test

--- Random.h
+++ Random.h
@@ -10,8 +10,7 @@
 
 std::mt19937 rnd(std::chrono::high_resolution_clock::now().time_since_epoch().count());
 
-inline unsigned int GetRandomNumber(unsigned int left_bound,
-                                    unsigned int right_bound) {
+inline unsigned int GetRandomNumber(unsigned int left_bound, unsigned int right_bound) {
     auto width = right_bound - left_bound + 1;
     return left_bound + rnd() % width;
 }
