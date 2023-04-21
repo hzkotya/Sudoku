b138912ee56ec8f4e081d5b0771fd0edda8cfe36
Георгий Возмилов <georgpv@mail.ru>
2023-04-22 00:50:47 +0300

    cosmetica tuda suda

---
+++ Random.h
@@ -0,0 +1,24 @@
+//
+// Created by georg on 22.04.2023.
+//
+
+#ifndef PROGRAM_RANDOM_H
+#define PROGRAM_RANDOM_H
+
+#include <chrono>
+#include <random>
+
+std::mt19937 rnd(std::chrono::high_resolution_clock::now().time_since_epoch().count());
+
+inline unsigned int GetRandomNumber(unsigned int left_bound,
+                                    unsigned int right_bound) {
+    auto width = right_bound - left_bound + 1;
+    return left_bound + rnd() % width;
+}
+
+inline unsigned int GetRandomNumber(unsigned int bound) {
+    return rnd() % bound;
+}
+
+
+#endif //PROGRAM_RANDOM_H
